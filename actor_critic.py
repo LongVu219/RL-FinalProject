9744277dbf34f542dda6e8cc21a342ba4a497f18
Truong <127490669+dangtruong-github@users.noreply.github.com>
2024-12-21 13:22:19 +0700

    refactor

--- actor_critic.py
+++ actor_critic.py
@@ -1,26 +1,27 @@
 import torch 
-from torch import nn 
-import random 
+from torch import nn
+import random
 from res_net import ResBlock
 
 
-class ActorCritic(nn.Module): 
+class ActorCritic(nn.Module):
     def __init__(
-        self, 
-        observation_shape, 
+        self,
+        observation_shape,
         action_shape
-    ): 
+    ):
         super(ActorCritic, self).__init__() 
         self.observation_shape = observation_shape
-        self.action_shape = action_shape 
+        self.action_shape = action_shape
 
         self.actor = nn.Sequential(
-            ResBlock(self.observation_shape), 
+            ResBlock(self.observation_shape),
             nn.Flatten(),
-            nn.Linear(in_features=self.observation_shape[0]*self.observation_shape[1]*self.observation_shape[2], out_features=128),
-            nn.ReLU(), 
+            nn.Linear(
+                in_features=self.observation_shape[0]*self.observation_shape[1]*self.observation_shape[2], out_features=128),
+            nn.ReLU(),
             nn.Linear(in_features=128, out_features=64), 
-            nn.ReLU(), 
+            nn.ReLU(),
             nn.Linear(64, self.action_shape),
             nn.Softmax(dim=-1)
         )
@@ -29,14 +30,13 @@ class ActorCritic(nn.Module):
             ResBlock(self.observation_shape), 
             nn.Flatten(),
             nn.Linear(in_features=self.observation_shape[0]*self.observation_shape[1]*self.observation_shape[2], out_features=128),
-            nn.ReLU(), 
-            nn.Linear(in_features=128, out_features=64), 
-            nn.ReLU(), 
-            nn.Linear(64, 1) 
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
         )
 
-
-    def forward(self): 
+    def forward(self):
         raise NotImplementedError
 
     def select_action(self, state): 
@@ -53,7 +53,3 @@ class ActorCritic(nn.Module):
             state_value = self.critic(state) 
 
             return action.item(), action_log_prob.item(), state_value.item() 
-    
-
-
-    
